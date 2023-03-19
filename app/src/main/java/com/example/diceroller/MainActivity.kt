72523ee90b63bb547616d2c66e70999d7a6d00b8
aakritiTBC <saakriti21@tbc.edu.np>
2023-03-19 12:48:59 +0545

    Dice roller

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -24,15 +24,16 @@ class MainActivity : AppCompatActivity() {
      * Roll the dice and update the screen with the result.
      */
     private fun rollDice() {
+        //lab 1
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val diceRoll2 = dice.roll()
+        //val diceRoll2 = dice.roll()
         // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
-        val resultTextView2: TextView = findViewById(R.id.textView2)
+        //val resultTextView2: TextView = findViewById(R.id.textView2)
         resultTextView.text = diceRoll.toString()
-        resultTextView2.text = diceRoll2.toString()
+        //resultTextView2.text = diceRoll2.toString()
     }
 }
 
